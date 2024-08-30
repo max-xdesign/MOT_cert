2e06d8a053259b88be11b2fa27d6b79d95bb6fef
Max Sadler <max.sadler@xdesign.com>
2024-08-30 17:00:24 +0100

    extended timeout

--- wdio.conf.js
+++ wdio.conf.js
@@ -103,7 +103,7 @@ exports.config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 60*1000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
